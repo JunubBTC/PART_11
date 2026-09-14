9c11a3c93451a65c52e792b305316fc967c08a99
21Bhang <uhuribhang211@gmail.com>
2026-09-14 15:10:55 +0300

    Configure Playwright base URL for CI

--- playwright.config.js
+++ playwright.config.js
@@ -2,6 +2,9 @@ const { defineConfig } = require('@playwright/test')
 
 module.exports = defineConfig({
   testDir: './e2e-tests',
+  use: {
+    baseURL: 'http://127.0.0.1:5001'
+  },
   webServer: {
     command: 'npm run build && npm run start-prod',
     url: 'http://127.0.0.1:5001',
